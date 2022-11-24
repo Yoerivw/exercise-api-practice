b26f6a70546cbfbb2aac3297a439297f80db1dca
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2022-11-24 09:59:06 +0200

    implemented auth0 with login logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import LoginButton from './assets/components/LoginButton';
+import LogoutButton from './assets/components/LogoutButton';
+import Profile from './assets/components/Profile';
+import {useAuth0} from '@auth0/auth0-react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const {isLoading} = useAuth0();
+
+  if(isLoading) return <div>is loading ...</div>;
+
+  useEffect(()=>{
+    fetch('https://wger.de/api/v2/exerciseinfo/')
+    .then((response) => {
+      if(!response.ok){
+        throw new Error(
+          `This is an HTTP error: the status is ${response.status}`
+        );
+      }
+      return response.json()})
+    .then((actualData) => {
+      setData(actualData.results)
+      setError(null);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
+  },[])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Exercise App</h1>
+    <LoginButton />
+    <LogoutButton />
+    <Profile />
+    {error && (
+      <div>{`There is a problem fetching your data - ${error} `}</div>
+    )}
+    <ul>
+      {data && data.map( data => {
+        return <li key={data.id}>
+          <h3>{data.name}</h3>
+        </li>
+      })}
+    </ul>
+      
     </div>
   )
 }
